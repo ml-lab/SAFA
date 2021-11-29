61626969fe3ba0a146c7c21a4ec1967b9e13d59b
wangqiulin5 <wangqiulin5@jd.com>
2021-11-29 11:48:45 +0800

    update

--- modules/generator.py
+++ modules/generator.py
@@ -38,9 +38,9 @@ class OcclusionAwareGenerator(nn.Module):
         for i in range(num_down_blocks):
             in_features = min(max_features, block_expansion * (2 ** (num_down_blocks - i)))
             out_features = min(max_features, block_expansion * (2 ** (num_down_blocks - i - 1)))
-            up_blocks.append(UpBlock2d(in_features*2, out_features, kernel_size=(3, 3), padding=(1, 1)))
+            #up_blocks.append(UpBlock2d(in_features, out_features, kernel_size=(3, 3), padding=(1, 1)))
             # We can also inject the driving 3DMM code to the upblock, see GADEUpBlock2d in util.py for details.
-            # up_blocks.append(GADEUpBlock2d(in_features*2, out_features, kernel_size=(3, 3), padding=(1, 1)))
+            up_blocks.append(GADEUpBlock2d(in_features, out_features, kernel_size=(3, 3), padding=(1, 1)))
         self.up_blocks = nn.ModuleList(up_blocks)
 
         self.bottleneck = torch.nn.Sequential()
@@ -49,7 +49,7 @@ class OcclusionAwareGenerator(nn.Module):
         for i in range(num_bottleneck_blocks):
             self.bottleneck.add_module('r' + str(i), ResBlock2d(in_features, kernel_size=(3, 3), padding=(1, 1)))
 
-        self.final = nn.Conv2d(block_expansion*2, num_channels, kernel_size=(7, 7), padding=(3, 3))
+        self.final = nn.Conv2d(block_expansion, num_channels, kernel_size=(7, 7), padding=(3, 3))
         self.estimate_occlusion_map = estimate_occlusion_map
         self.num_channels = num_channels
 
@@ -58,8 +58,8 @@ class OcclusionAwareGenerator(nn.Module):
         else:
             self.blend_downsample = AntiAliasInterpolation2d(1, blend_scale)
 
-        self.fc_1 = nn.Linear(159, in_features)
-        self.fc_2 = nn.Linear(159, in_features)
+        self.fc_1 = nn.Linear(1280, in_features)
+        self.fc_2 = nn.Linear(1280, in_features)
 
         self.context_atten_layer = ContextualAttention(ksize=3, stride=1, rate=2, fuse_k=3, softmax_scale=10, fuse=True)
 
@@ -89,13 +89,10 @@ class OcclusionAwareGenerator(nn.Module):
     def forward(self, source_image, kp_driving=None, kp_source=None, render_ops=None, 
                       blend_mask=None, driving_image=None, driving_features=None):
 
-        # U-net encoding part
-        source_features = []
+        # Encoder
         out = self.first(source_image)
-        source_features.append(out)
         for i in range(len(self.down_blocks)):
             out = self.down_blocks[i](out)
-            source_features.append(out)
 
         # Transforming feature representation according to deformation and occlusion
         output_dict = {}
@@ -129,13 +126,8 @@ class OcclusionAwareGenerator(nn.Module):
             output_dict["deformed"] = self.deform_input(source_image, deformation)
 
         # Geometrically-Adaptive Denormalization (GADE) Layer
-        code = driving_features['code']
-        # 1. normalize each part of the code separately
-        code[:, 0:100] = code[:, 0:100] * 0.02
-        code[:, 100:150] = code[:, 100:150] * 0.03
-        code[:, 156] = code[:, 156] * 0.25
+        code = driving_features['feature_vec']
 
-        # 2. geometric feature injection
         r = self.fc_1(code).unsqueeze(-1).unsqueeze(-1).expand_as(out)
         beta = self.fc_2(code).unsqueeze(-1).unsqueeze(-1).expand_as(out)
         addin_driving = r * out + beta
@@ -164,14 +156,9 @@ class OcclusionAwareGenerator(nn.Module):
         out = torch.cat([ca_out, dilated_out], dim=1)
         out = self.concat_conv(out)
 
-        # U-net decoding part
         for i in range(len(self.up_blocks)):
-            deformed_source = self.deform_input(source_features[len(self.down_blocks)-i], deformation)
-            out = torch.cat([deformed_source, out], dim=1)
-            out = self.up_blocks[i](out)
-            #out = self.up_blocks[i](out, code)   # in case of GADEUpBlock2d
-        deformed_source = self.deform_input(source_features[0], deformation)
-        out = torch.cat([deformed_source, out], dim=1)
+            #out = self.up_blocks[i](out)
+            out = self.up_blocks[i](out, code)
 
         out = self.final(out)
         out = F.sigmoid(out)
